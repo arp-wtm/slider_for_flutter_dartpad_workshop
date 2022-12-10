41af921245c12fb5f1ddac36011a2a46df6533bd
Antonella Rotondo <33467518+arp-wtm@users.noreply.github.com>
2022-12-10 15:41:19 +0100

    Create solution.dart

---
+++ step_02/solution.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
+class IntroSlider extends StatefulWidget {
+  @override
+  IntroSliderState createState() => IntroSliderState();
+}
+
+class IntroSliderState extends State<IntroSlider> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     color: Colors.green
+    );
+  }
+}
