a27cd027c0fd0960fa8682a40c41a19ea9e55df7
Antonella Rotondo <33467518+arp-wtm@users.noreply.github.com>
2022-12-10 17:23:27 +0100

    Update snippet.dart

--- step_05/snippet.dart
+++ step_05/snippet.dart
@@ -17,16 +17,14 @@ class IntroSlider extends StatefulWidget {
 class _IntroSliderState extends State<IntroSlider> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(children: [
+    return PageView(children: [
       EachPage("First Screen", 'https://images.yourstory.com/cs/1/b3c70442-ab5e-11e8-8691-f70342131e20/flutter-application-development-info21550223369792.png'),
       EachPage("Second Screen", 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png'),
       EachPage("Third Screen", 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Iphone_4G-3_grey_screen.png'),
-      ]),
-    );
+    ]);
   }
 }
-
+    
 class EachPage extends StatelessWidget {
   final String message;
   final String image;
@@ -36,25 +34,14 @@ class EachPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network(
-                image,
-                width: 200,
-                height: 200,
-              ),
-              Text(
-                message,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
+      body: Container( // TO DO Alignment
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              image,
+            ),
+            Text(message) // TO DO Styling
+          ],
         ),
       ),
     );
