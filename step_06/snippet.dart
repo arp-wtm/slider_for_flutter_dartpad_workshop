258a4e9f2d14f9c10e1bf196755f9b576b3633b5
Antonella Rotondo <33467518+arp-wtm@users.noreply.github.com>
2022-12-10 18:20:16 +0100

    Create snippet.dart

---
+++ step_06/snippet.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: IntroSlider());
+  }
+}
+
+class IntroSlider extends StatefulWidget {
+  @override
+  State<IntroSlider> createState() => _IntroSliderState();
+}
+
+class _IntroSliderState extends State<IntroSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: // TO DO add stack
+        children: [
+          PageView(children: [
+            EachPage("First Screen", 'https://images.yourstory.com/cs/1/b3c70442-ab5e-11e8-8691-f70342131e20/flutter-application-development-info21550223369792.png'),
+            EachPage("Second Screen", 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png'),
+            EachPage("Third Screen", 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Iphone_4G-3_grey_screen.png'),
+          ]),
+         // TO DO add center aligment 
+            // TO DO add a row
+              children: [
+                // TO DO add Expanded 
+                  //TO DO add padding
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Register'),
+                    ),
+                  ),
+                ),
+                // TO DO add Expanded 
+                  // TO DO add padding
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Login'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EachPage extends StatelessWidget {
+  final String message;
+  final String image;
+
+  const EachPage(this.message, this.image, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                image,
+                width: 200,
+                height: 200,
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
