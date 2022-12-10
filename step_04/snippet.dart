ba0aa9da007f33cf94aa1dc1a2d7ad1343f39a0b
Antonella Rotondo <33467518+arp-wtm@users.noreply.github.com>
2022-12-10 17:02:47 +0100

    Update snippet.dart

--- step_04/snippet.dart
+++ step_04/snippet.dart
@@ -18,32 +18,9 @@ class _IntroSliderState extends State<IntroSlider> {
   @override
   Widget build(BuildContext context) {
     return PageView(children: [
-      EachPage("First Screen", 'https://images.yourstory.com/cs/1/b3c70442-ab5e-11e8-8691-f70342131e20/flutter-application-development-info21550223369792.png'),
-      EachPage("Second Screen", 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png'),
-      EachPage("Third Screen", 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Iphone_4G-3_grey_screen.png'),
+      // TO DO instance 3 EachPage container with message and image attribute
     ]);
   }
 }
     
-class EachPage extends StatelessWidget {
-  final String message;
-  final String image;
-
-  const EachPage(this.message, this.image, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Image.network(
-              image,
-            ),
-            Text(message)
-          ],
-        ),
-      ),
-    );
-  }
-}
+// TO DO class EachPage 
